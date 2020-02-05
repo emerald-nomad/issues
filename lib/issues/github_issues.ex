0b09a276fbda33e6b6d15120540edd98d0b01a48
terrell-greene <33211911+terrell-greene@users.noreply.github.com>
2020-02-04 20:04:41 -0600

    Fixed bug for handle_response function

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -12,7 +12,7 @@ defmodule Issues.GithubIssues do
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response({ :ok, %{status_code: 200, body: body}}) do
+  def handle_response({ _, %{status_code: status_code, body: body}}) do
     { 
       status_code |> check_for_error(),
       body        |> Poison.Parser.parse!()
